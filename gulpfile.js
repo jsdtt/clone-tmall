b9e94c02d8fa0869929d6475f58e6b6f4f3700a8
jsdtt <591713854@qq.com>
2017-01-16 12:49:16 +0800

    init

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+const gulp = require('gulp');
+const postcss = require('gulp-postcss');
+const sass = require('gulp-sass');
+
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+
+gulp.task('css', function() {
+  const processors = [autoprefixer, cssnano]
+  return gulp.src('./src/*.scss')
+    .pipe(sass().on('error', sass.logError)).
+    .pipe(postcss(processors))
+    .pipe(gulp.dest('./dest'))
+})
