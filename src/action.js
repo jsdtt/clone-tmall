474d06aa8aa633f1226d470400f945b9c6d4afe7
jsdtt <591713854@qq.com>
2017-01-17 13:50:31 +0800

    Top-nav-down

--- src/action.js
+++ src/action.js
@@ -6,7 +6,9 @@ function addHover() {
   let mytb = document.getElementById('myTaobao')
   let myTaobaoPanel = document.getElementById('menubd'),
       myFavority = document.getElementById('myFavority'),
-      myFavorityPanel = document.getElementById('myFavorityPanel')
+      myFavorityPanel = document.getElementById('myFavorityPanel'),
+      seller = document.getElementById('seller'),
+      sellerPanel = document.getElementById('sellerPanel')
   let onPlam = false
   mytb.addEventListener('mouseover', function() {
     mytb.className = "menu-hd hover"
@@ -32,5 +34,18 @@ function addHover() {
   })
   myFavority.addEventListener('mouseout', function() {
     myFavority.className = "menu-hd";
+  })
+
+  seller.addEventListener('mouseover', function() {
+    seller.className = "menu-hd hover"
+  })
+  sellerPanel.addEventListener('mouseover', function() {
+    seller.className = "menu-hd hover"
+  })
+  sellerPanel.addEventListener('mouseout', function() {
+    seller.className = "menu-hd";
+  })
+  seller.addEventListener('mouseout', function() {
+    seller.className = "menu-hd";
   })
 }
