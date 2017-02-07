4a70efe86144111aef098feec26c361563fc44a5
jsdtt <591713854@qq.com>
2017-02-07 21:04:46 +0800

    添加gulp-connect

--- gulpfile.js
+++ gulpfile.js
@@ -5,8 +5,9 @@ var babel = require("gulp-babel");    // 用于ES6转化ES5
 
 const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
+const connect = require('gulp-connect');
 
-gulp.task('css', function() {
+gulp.task('css', function () {
   const processors = [autoprefixer, cssnano]
   return gulp.src('./src/*.scss')
     .pipe(sass().on('error', sass.logError))
@@ -18,7 +19,12 @@ gulp.task('toes5', function () {
     .pipe(babel())
     .pipe(gulp.dest('./dest'))
 })
-gulp.task('watch-css', function() {
-  gulp.watch('./src/style.scss',['css'])
-  gulp.watch('./src/action.js',['toes5'])
+
+gulp.task('connect', function() {
+  connect.server();
+});
+gulp.task('watch', function() {
+  gulp.watch('./src/style.scss', ['css'])
+  gulp.watch('./src/action.js', ['toes5'])
 })
+gulp.task('watch-css', ['watch','connect'])
