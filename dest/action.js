fb49c7238e3e2852f7c4e7a73f1a44515b6c33d0
jsdtt <591713854@qq.com>
2017-02-07 21:05:13 +0800

    添加layzr.js延迟加载图片

--- dest/action.js
+++ dest/action.js
@@ -1,5 +1,17 @@
 'use strict';
 
+// var Layzr = require('layzr.js') 
+
+var instance = Layzr();
+
+// start it up, when the DOM is ready
+
+document.addEventListener('DOMContentLoaded', function (event) {
+  instance.update() // track initial elements
+  .check() // check initial elements
+  .handlers(true); // bind scroll and resize handlers
+});
+
 window.onload = function () {
   showPanel();
   showNormalNvaPannel();
