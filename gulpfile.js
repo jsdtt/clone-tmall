5c61dae42e9db312471a17c7117cb11933fcd081
jsdtt <591713854@qq.com>
2017-02-10 10:00:14 +0800

    gulp auto reload

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ const postcss = require('gulp-postcss');
 const sass = require('gulp-sass');
 var babel = require("gulp-babel");    // 用于ES6转化ES5
 
+const uglify = require('gulp-uglify')
 const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
 const connect = require('gulp-connect');
@@ -13,18 +14,31 @@ gulp.task('css', function () {
     .pipe(sass().on('error', sass.logError))
     .pipe(postcss(processors))
     .pipe(gulp.dest('./dest'))
+    // .pipe(connect.reload())
 })
 gulp.task('toes5', function () {
   return gulp.src('./src/*.js')
     .pipe(babel())
+    .pipe(uglify())
     .pipe(gulp.dest('./dest'))
+    // .pipe(connect.reload())
 })
 
 gulp.task('connect', function() {
-  connect.server();
+  connect.server({
+    livereload: true ,//自动刷新
+    root: './'
+  })
 });
+
+gulp.task('refresh', function() {
+  gulp.src('./*.html').pipe(connect.reload())
+})
+
 gulp.task('watch', function() {
-  gulp.watch('./src/style.scss', ['css'])
-  gulp.watch('./src/action.js', ['toes5'])
+  gulp.watch('./src/style.scss', ['css','refresh'])
+  gulp.watch('./src/action.js', ['toes5','refresh'])
+  gulp.watch('./*.html', ['refresh'])
+  // gulp.src('./*.html').pipe(connect.reload())
 })
 gulp.task('watch-css', ['watch','connect'])
