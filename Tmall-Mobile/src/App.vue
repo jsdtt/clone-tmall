ee3ca81dee742412466526618ac7961a00a953c0
jsdtt <591713854@qq.com>
2017-02-19 21:14:48 +0800

    layout set

--- Tmall-Mobile/src/App.vue
+++ Tmall-Mobile/src/App.vue
@@ -1,23 +1,28 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <router-view></router-view>
   </div>
 </template>
-
 <script>
 export default {
   name: 'app'
 }
 </script>
-
-<style>
+<style lang="scss">
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+* {
+  padding: 0;
+  margin: 0;
+}
+a{
+    text-decoration: none;
+    color: #000;
+}
+body {
+  color: #999;
 }
 </style>
--- Tmall-Mobile/src/router/index.js
+++ Tmall-Mobile/src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from 'components/Hello'
+import CategoryMenu from 'components/header/CategoryNva/CategoryMenu'
 
 Vue.use(Router)
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello
+      name: 'CategoryMenu',
+      component: CategoryMenu
     }
   ]
 })
