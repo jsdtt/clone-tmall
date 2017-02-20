c01c4f0aacb2e29f21ac15004989361a32983b82
jsdtt <591713854@qq.com>
2017-02-20 22:24:42 +0800

    vue-resource

--- Tmall-Mobile/src/router/index.js
+++ Tmall-Mobile/src/router/index.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Resource from 'vue-resource'
+
 import CategoryMenu from 'components/header/CategoryNva/CategoryMenu'
 
 Vue.use(Router)
+Vue.use(Resource)
 
 export default new Router({
   routes: [
