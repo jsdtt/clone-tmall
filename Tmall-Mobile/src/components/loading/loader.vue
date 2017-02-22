cc65606fe1a124106660841b293a82606fcb7cc4
jsdtt <591713854@qq.com>
2017-02-22 11:26:42 +0800

    loader component

---
+++ Tmall-Mobile/src/components/loading/loader.vue
@@ -0,0 +1,36 @@
+<template>
+  <transition name="loader">
+    <div class="loader">
+      <div class="loader-inner">
+        <div class="loader-line-wrap">
+          <div class="loader-line"></div>
+        </div>
+        <div class="loader-line-wrap">
+          <div class="loader-line"></div>
+        </div>
+        <div class="loader-line-wrap">
+          <div class="loader-line"></div>
+        </div>
+        <div class="loader-line-wrap">
+          <div class="loader-line"></div>
+        </div>
+        <div class="loader-line-wrap">
+          <div class="loader-line"></div>
+        </div>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script></script>
+
+<style scoped lang="scss">
+  @import 'loader-style.css';
+
+  .loader-enter-active, .loader-leave-active {
+    transition: opacity .5s
+  }
+  .loader-enter, .loader-leave-active {
+    opacity: 0
+  }
+</style>
